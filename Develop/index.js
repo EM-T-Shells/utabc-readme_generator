ad77444bf0f4927d0a4d68448992f77ef5ea4620
Gokhan Emre Taskale <taskaleg@gmail.com>
2022-11-17 11:48:34 -0600

    template complete. installing node packages.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,196 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer.prompt(
+   [
+      {
+         type: 'input',
+         message: "What's the project Title?",
+         name: 'title',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "Project Description?",
+         name: 'description',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Description Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "Installation directions?",
+         name: 'installation',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "Provide instructions and examples for use.",
+         name: 'usage',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "Credits: List collaborators, if any.",
+         name: 'credits',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "License type?",
+         name: 'license',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "GitHub username?",
+         name: 'github',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      },
+      {
+         type: 'input',
+         message: "E-Mail?",
+         name: 'email',
+         validate: (value)=>
+         {
+            if(value)
+            {
+               return true
+            }
+            else
+            {
+               return 'Value Required'
+            }
+         }
+      }
+   ]
+)
+.then((
+{
+   title,
+   description,
+   installation,
+   usage,
+   credits,
+   license,
+   github,
+   email
+}) =>
+   {
+      //template
+      const template =
+      `# ${title}
+
+      * [description](#description)
+      * [installation](#installation)
+      * [usage](#usage)
+      * [credits](#credits)
+      * [license](#license)
+
+      ## description
+      ${description}
+      ## installation
+      ${installation}
+      ## usage
+      ${usage}
+      ## credits
+      ${credits}
+      ## license
+      ${license}
+
+      # Contact
+      * github:${github}
+      * email:${email}`;
+      *
+      //function to create readme w. fs
+      createNewFile(title, template);
+
+   }
+);
+
+//createNewFile Fx
+function createNewFile(fileName, data)
+{
+   fs.writeFile(`./${fileName.toLowerCase().split('').join('')}.md`, data)
+      if(err){
+      console.log(err)
+   }
+   console.log('README generated')
+
+}
+
+//instal node packages
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
